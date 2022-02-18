81cc7ea54b9c335c70776bac02a74a27237d7c14
MrSumada <ricksamericana.com>
2022-02-18 15:42:21 -0500

    wrote Employee.js to pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,57 +1,47 @@
 const Employee = require("../lib/Employee");
 
 
-test("creates an employee object", () => {
-    const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
+// test("creates an employee object", () => {
+//     const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
 
-    expect(employee.name).toBe("Adam");
-    expect(employee.ID).toEqual(expect.any(Number));
-    expect(employee.email.length).toBeGreaterThan(0);
-    expect(employee.role).toBe("Employee")
+//     expect(employee.name).toBe("Adam");
+//     expect(employee.ID).toEqual(expect.any(Number));
+//     expect(employee.email.length).toBeGreaterThan(0);
+//     expect(employee.role).toBe("Employee")
 
     // expect(employee.getName()).toHaveProperty("name");
-});
+// });
 
 describe('Employee', () => {
     describe('getName', () => {
-        it('should retrieve a name given string', () => {
-        const employee = new Employee("Adam");
+        it('should retrieve a name given an Obj', () => {
+            const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
 
-        // const result = new Employee().getName(employee);
-        expect(employee.getName()).toBe("Adam");
-
-        // expect(result).toBe("Hello World!");
-        // expect(result).toEqual(expect.stringContaining('Hello World!'));
+            expect(employee.getName()).toBe("Adam");
         });
     })
 
-//     describe('getId', () => {
-//         it('should retrieve an ID given number', () => {
-//             const employee = new Employee("Adam", 20, "adam@gmail.com");
+    describe('getId', () => {
+        it('should retrieve an ID number given an Obj', () => {
+            const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
 
-//             const result = new Employee().getId(num);
+            expect(employee.getId()).toEqual(20);
+        })
+    })
 
-//             expect(result).toEqual(1);
-//         })
-//     })
+    describe('getEmail', () => {
+        it('should retrieve an email given an Obj', () => {
+            const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
 
-//     describe('getEmail', () => {
-//         it('should retrieve an email given a string', () => {
-//             const str = "Adam.com";
+            expect(employee.getEmail()).toBe("adam@gmail.com");
+        })
+    })
 
-//             const result = new Employee().getEmail("str");
+    describe('getRole', () => {
+        it('should retrieve a role given an Obj', () => {
+            const employee = new Employee("Adam", 20, "adam@gmail.com", "Employee");
 
-//             expect(result).toBe("Adam.com");
-//         })
-//     })
-
-//     describe('getRole', () => {
-//         it('should retrieve an email given a string', () => {
-//             const str = "Employee";
-
-//             const result = new Employee().getRole(str);
-
-//             expect(result).toBe("Employee");
-//         })
-//     })
+            expect(employee.getRole()).toBe("Employee");
+        })
+    })
 })
